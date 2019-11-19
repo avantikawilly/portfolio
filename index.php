0998f918d81051b89b259a20ecb722dc9564ea83
avantikawilly <avantika.willy@gmail.com>
2019-11-19 16:29:18 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+        <?php echo '<p>Hello World! Here are all my links! Avantika Willy Period 1</p>'; ?> 
+  </head>
+  <body>
+     <a href="index.html">Home</a>
+     <a href="Willy_AboutMe.html">About Me</a>
+     <a href="Willy_Projects.html">Projects</a>
+     <a href="Willy_Resume.html">Resume</a>
+     <a href="Willy_ContactMe.html">Contact Me</a>
+  </body>
+</html>
